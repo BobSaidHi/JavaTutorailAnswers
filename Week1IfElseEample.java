892dae1f5e5691bd8a42756be84df63780284860
BobSaidHi <1019928@student.salinasuhsd.org>
2019-09-09 16:33:10 -0700

    Create Week1IfElseEample.java

---
+++ Week1IfElseEample.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+
+    //Create a condition
+    boolean condition1 = true;
+
+    //Is the condition true?
+    if(condition1 == true) {
+      //Do this
+      System.out.println("condition1 is true");
+    }
+
+    //What if none of the above is true?
+    else {
+      //Do this
+      System.out.println("condition1 is not true");
+    }
+
+  }
+}
